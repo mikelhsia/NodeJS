c5f4a6ddea90037dbbf4ca5967b5516f25cc5b06
Michael <michael@MichaeldeMacBook-Pro.local>
2018-11-02 16:54:46 +0800

    [Node JS] Remove unnecessary files

--- node_js_by_example/chapFour/modelViewControllerLayers/static/js/app.js
+++
@@ -1,4 +0,0 @@
-/* *
- * This is our client-side JavaScript. As seen in a previous chapter, 
- * it is produced by the Gulp setup.
- * */
--- node_js_by_example/chapFour/modelViewControllerLayers/static/js/ractive.js
+++
@@ -1,5 +0,0 @@
-/* *
- * This is the framework that we will use in the client-side.
- * More about this will be discussed in the next few sections
- * */
-
