d7ba5b269c6c11c9e2581ccb0d1df0b07f815d30
Michael <michael@MichaeldeMBP.lan>
2018-12-01 00:39:45 +0800

    [Node JS] 1. First lesson for async module

---
+++ learningNode/chap3/ex3-6.js
@@ -0,0 +1,67 @@
+var fs = require('fs'),
+    async = require('async'),
+    _dir = './data/';
+
+// TODO: 1. You can't call callback in waterfall multiple times
+// TODO: 2. Somehow fs.stat(_dir+file ...) doesn't get triggered.
+
+var writeStream = fs.createWriteStream('./log.txt', {
+    'flags': 'a',
+    'encoding': 'utf8',
+    'mode': 0666
+});
+
+async.waterfall([
+    function readDir(callback) {
+        console.log('readDir');
+        fs.readdir(_dir, function (err, files) {
+            console.log(files);
+            callback(err, files);
+        });
+    },
+    function loopFiles(files, callback) {
+        console.log('looping Files');
+        files.forEach(function (name) {
+            console.log('loop file');
+            console.log(name);
+            callback(null, name);
+        });
+    },
+    function checkFile(file, callback) {
+        console.log('checkFile');
+        console.log(_dir+file);
+        fs.stat(_dir + file, function(err, stats){
+            console.log(_dir+file + ' existed');
+            callback(err, stats, file);
+        });
+    },
+    function readData(stats, file, callback) {
+        console.log('reading data');
+        if (stats.isFile()) {
+            console.log('reading file: ' + _dir + file);
+            fs.readFile(_dir + file, 'utf8', function(err, data) {
+                callback(err, file, data);
+            });
+        }
+    },
+    function modify(file, text, callback) {
+        var adjdata = text.replace(/somecompany\.com/g, 'burningbird.net');
+        callback(null, file, adjdata);
+    },
+    function writeData(file, text, callback) {
+        fs.writeFile(_dir + file, text, function(err) {
+            callback(err, file);
+        });
+    },
+    function logChange(file, callback) {
+        writeStream.write('changed ' + file + '\n', 'utf8', function (err) {
+            callback(err);
+        });
+    }
+], function (err) {
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log('modified files');
+    }
+});
