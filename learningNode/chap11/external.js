76526b39c3b5aef12f65b74c7c0d1e95c1509a24
michael <mikelhsia@hotmail.com>
2018-12-27 16:22:06 +0800

    [Node JS] 1. Finish chap 11 2. Experiencing multiple other modules 3. Wrapping up this book

---
+++ learningNode/chap11/external.js
@@ -0,0 +1,7 @@
+(function (exports, require, module, __fielname, __dirname) {
+    var concatArray = function(str, arry) {
+        return arry.map(function(element) {
+            return str + ' ' + element;
+        });
+    }
+});
